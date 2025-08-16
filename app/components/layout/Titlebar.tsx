851b68cda6e6ee1b8bdb5466b9ec99e91c5e3f5a
Yuanzhou <cai.yz@outlook.com>
2025-08-17 00:00:54 +0800

    add custom titlebar

--- app/components/layout/Titlebar.tsx
+++ app/components/layout/Titlebar.tsx
@@ -1,12 +1,7 @@
 "use client";
 
+import { WindowTitlebar } from "../titlebar";
+
 export default function Titlebar() {
-  return (
-    <div 
-      data-tauri-drag-region
-      className="h-7 w-full bg-background flex items-center justify-center fixed top-0 left-0 z-50"
-    >
-      {/* Empty titlebar - draggable area */}
-    </div>
-  );
+  return <WindowTitlebar title="" />;
 }
---
+++ app/components/titlebar/index.ts
@@ -0,0 +1,2 @@
+export { default as WindowTitlebar } from './WindowTitlebar';
+export * from './windowControls';
